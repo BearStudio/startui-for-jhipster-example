4541048c0392cc294e7bac1db64595b7e4ac34a4
Thomas Halipré <thomas@bearstudio.fr>
2022-08-10 17:33:55 +0200

    Initial version of startuiforjhipster generated by generator-jhipster@7.9.2

---
+++ src/main/java/fr/bearstudio/startuiforjhipster/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package fr.bearstudio.startuiforjhipster.security;
---
+++ src/main/java/fr/bearstudio/startuiforjhipster/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package fr.bearstudio.startuiforjhipster.service.dto;
